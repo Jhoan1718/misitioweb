6f4274db60e8c574352b06e6c26b10ce12bbef0f
Sebastian <sr.jhoan16@gmail.com>
2022-04-21 18:43:24 -0500

    añadiendo los transforms

--- js/homecenter.js
+++ js/homecenter.js
@@ -10,12 +10,37 @@ botonCerrar.addEventListener("click", ocultarForm);
 function mostrarForm() {
     div4.classList.add("transform5");
     div4.classList.remove ("transform4");
-    botonCerrar.classList.add("otonCerrarVisible");
+    botonCerrar.classList.add("botonCerrarVisible");
     botonCerrar.classList.remove("botonCerrar");
 }
 function ocultarForm(){
     div4.classList.remove("transform5");
     div4.classList.add("transform4");
+    botonCerrar.classList.remove("botonCerrarVisible");
+    botonCerrar.classList.add("botonCerrar");
+
+}
+
+
+
+
+let botonForm2 = document.getElementById("botonForm2");
+let div5 = document.getElementById ("div5");
+
+let botonCerrar2 = document.getElementById("botonCerrar2");
+
+botonForm2.addEventListener("click", mostrarForm2);
+botonCerrar.addEventListener("click", ocultarForm2);
+
+function mostrarForm2(){
+    div5.classList.add("presentacion2");
+    div5.classList.remove("presentacion");
+    botonCerrar2.classList.add("botonCerrarVisible");
+    botonCerrar.classList.remove("botonCerrar");
+}
+function ocultarForm2(){
+    div4.classList.remove("presentacion2");
+    div4.classList.add("presentacion");
     botonCerrar.classList.remove("botonCerrarVisible");
     botonCerrar.classList.add("botonCerrar");
 
