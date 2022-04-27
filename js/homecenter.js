ab98c9edd546265648cd3cf1cd133cbdbeb5f6bd
Sebastian <sr.jhoan16@gmail.com>
2022-04-27 01:12:50 -0500

    añadiendo lo de home

--- js/homecenter.js
+++ js/homecenter.js
@@ -1,78 +1,72 @@
-let botonForm = document.getElementById("botonForm");
+ let botonForm = document.getElementById("botonForm");
 let div4 = document.getElementById("div4");
 
 
-let botonCerrar = document.getElementById("botonCerrar");
+let botonCerrar1 = document.getElementById("botonCerrar1");
 
 botonForm.addEventListener("click", mostrarForm);
 
-botonCerrar.addEventListener("click", ocultarForm);
+botonCerrar1.addEventListener("click", ocultarForm);
 
 function mostrarForm() {
     div4.classList.add("transform5");
     div4.classList.remove ("transform4");
-    botonCerrar.classList.add("botonCerrarVisible");
-    botonCerrar.classList.remove("botonCerrar");
+    botonCerrar1.classList.add("botonCerrarVisible");
+    botonCerrar1.classList.remove("botonCerrar");
 }
 function ocultarForm(){
     div4.classList.remove("transform5");
     div4.classList.add("transform4");
-    botonCerrar.classList.remove("botonCerrarVisible");
-    botonCerrar.classList.add("botonCerrar");
+    botonCerrar1.classList.remove("botonCerrarVisible");
+    botonCerrar1.classList.add("botonCerrar1");
+
+}
+ 
+
+"use strict"
+
+let botonOpen = document.getElementById("chat-box-disable");
+let botonCerrar = document.getElementById("botonCerrar");
+let botonOpenHeader = document.getElementById("header-movil-open");
+let botonCerrarHeader = document.getElementById("boton-cerrar-header");
+let Header = document.getElementById("header");
+
+botonOpen.addEventListener("click", dezplegarCaja);
+botonCerrar.addEventListener("click", cerrarCaja);
+
+function dezplegarCaja(){
+
+    botonOpen.classList.add("chat-box-disable-in");
+    botonOpen.classList.remove("chat-box-disable-out");
+    document.getElementById("chat-box").classList.add("chat-box-in");
+    document.getElementById("chat-box").classList.remove("chat-box-out");
 
 }
 
+function cerrarCaja(){
+    
+    botonOpen.classList.remove("chat-box-disable-in");
+    botonOpen.classList.add("chat-box-disable-out");
+    document.getElementById("chat-box").classList.remove("chat-box-in");
+    document.getElementById("chat-box").classList.add("chat-box-out");
+};
 
+botonOpenHeader.addEventListener("click", dezplegarCajaHeader);
+botonCerrarHeader.addEventListener("click", cerrarCajaHeader);
 
+function dezplegarCajaHeader(){
 
+    document.getElementById("header-movil").classList.add("contenedor-barra-busqueda-movil");
 
+    document.getElementById("header-movil").classList.remove("contenedor-barra-busqueda-movil-out");
 
-const buttonBusqueda = document.querySelector('.fa-solid fa-magnifying-glass');
-const busqueda = document.querySelector('.buscar')
-const contenido = document.querySelector('div5')
-
-buttonBusqueda.addEventListener('click', () => {
-    busqueda.classList.toggle('buscar')
-});
-
-ocultarbutton.addEventListener('click', buscar);
-
-function buscar (){
-    contenido.classList.add('presentacion2')
-    contenido.classList.remove ('presentacion')
-}
-salirBot.addEventListener('click', );
-
-
-function salir (){
-
+    document.getElementById("header").style.display="none";
 }
 
+function cerrarCajaHeader(){
+    document.getElementById("header-movil").classList.remove("contenedor-barra-busqueda-movil");
 
+    document.getElementById("header-movil").classList.add("contenedor-barra-busqueda-movil-out");
 
-
-
-
-
-
-let botonForm2 = document.getElementById("botonForm2");
-let div5 = document.getElementById ("div5");
-
-let botonCerrar2 = document.getElementById("botonCerrar2");
-
-botonForm2.addEventListener("click", mostrarForm2);
-botonCerrar.addEventListener("click", ocultarForm2);
-
-function mostrarForm2(){
-    div5.classList.add("presentacion2");
-    div5.classList.remove("presentacion");
-    botonCerrar2.classList.add("botonCerrarVisible");
-    botonCerrar.classList.remove("botonCerrar");
-}
-function ocultarForm2(){
-    div4.classList.remove("presentacion2");
-    div4.classList.add("presentacion");
-    botonCerrar.classList.remove("botonCerrarVisible");
-    botonCerrar.classList.add("botonCerrar");
-
+    document.getElementById("header").style.display="flex";
 }
