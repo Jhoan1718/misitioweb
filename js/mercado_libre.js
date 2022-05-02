cb086ab2ea73b174f758e266635f929bd74b98b3
Sebastian <sr.jhoan16@gmail.com>
2022-05-02 11:33:39 -0500

    añadiendo mercadol

--- js/mercado_libre.js
+++ js/mercado_libre.js
@@ -1,135 +1,158 @@
-class Motocicleta {
-    constructor (marca, modelo, precio, img){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.precio = precio;
-        this.img = img;
-       
+
+
+class Automovil {
+    constructor (marca, modelo, precio, imagen) {
+        this.Marca = marca ;
+        this.Modelo = modelo;
+        this.Precio = precio;
+        this.Imagen = imagen;
     }
+}
+let auto = new Automovil (" Apple Iphone 12", "̶$6̶`̶0̶0̶0̶.̶0̶0̶0̶", "$ 5'000.000", "img/Iphone1.jpg");
+
+function validarMazda () {
+    alert(auto.Marca + "" + auto.Modelo + ""  + " El automovil que necesitas¡¡");
     
+    mostrarVehiculo();
 }
-let moto = new  Motocicleta("yamaha", " 3 ", " $ 20.000.000", "img/moto_Yama");
-function validarYamaha() {
+function mostrarVehiculo() {
+    let contentPrincipal = document.getElementById("main_content");
+    let contentAuto   = document.createElement("div");
+    contentPrincipal.appendChild(contentAuto);
+    contentAuto.setAttribute("class", "div");
     
 
-    mostrarMoto();
-}
-function mostrarMoto (){
-    let contentPrincipal  = document.getElementById("moticos");
-    let contentMoto = document.createElement("div");
-    contentPrincipal.appendChild(contentMoto);
-
-    let marcaMoto = document.createElement("label");
-    contentMoto.appendChild(marcaMoto);
-    let textMarca = document.createTextNode(moto.marca)
-    marcaMoto.appendChild(textMarca);
-
-    
-    let modeloMoto = document.createElement("label");
-    contentMoto.appendChild(modeloMoto);
-    let textModelo = document.createTextNode(moto.modelo)
-    modeloMoto.appendChild(textModelo);
-
-    
-    let precioMoto = document.createElement("label");
-    contentMoto.appendChild(precioMoto);
-    let textPrecio = document.createTextNode(moto.precio)
-    precioMoto.appendChild(textPrecio);
-
- 
-    let imagenMoto = document.createElement("img");
-    contentMoto.appendChild(imagenMoto);
-    imagenMoto.setAttribute("src", moto.img);
-}
-let Deportiva = new Motocicleta ("deportiva", " 4 ", " $ 25.800.000", "img/moto_Deportiva.jpg");
-
-function validarDeportiva () {
+    let marcaAuto = document.createElement("label");
+    contentAuto.appendChild(marcaAuto);
+    let textMarca = document.createTextNode(auto.Marca);
+    marcaAuto.appendChild(textMarca);
+    marcaAuto.setAttribute("class", "label1");
 
 
-    mostrarDeportiva ();
+    let modeloAuto  =  document.createElement("label");
+    contentAuto.appendChild(modeloAuto);
+    let textModelo  =  document.createTextNode(auto.Modelo);
+    modeloAuto.appendChild(textModelo);
+    modeloAuto.setAttribute("class", "label2");
+
+    let precioAuto = document.createElement("label");
+    contentAuto.appendChild(precioAuto);
+    let textAuto = document.createTextNode(auto.Precio);
+    precioAuto.appendChild(textAuto);
+    precioAuto.setAttribute("class", "label3");
+
+
+    let imagenAuto  = document.createElement("img");
+    contentAuto.appendChild(imagenAuto);
+    imagenAuto.setAttribute("src", auto.Imagen);
+    imagenAuto.setAttribute("class", "img");
 }
 
-function mostrarDeportiva () {
-    let contenedorPrinDeportiva = document.getElementById("moticos");
-    let contentDeportiva    = document.createElement("div");
-    contenedorPrinDeportiva.appendChild(contentDeportiva);
+let toyota = new Automovil ("Apple Iphone 12 pro", "En 36x $141.667 ", "$ 5'100.000", "img/iphone2.jpg");
 
-    let marcaDeportiva = document.createElement("label");
-    contentDeportiva.appendChild(marcaDeportiva);
-    let textDeportivaMarca = document.createTextNode(Deportiva.Marca);
-    marcaDeportiva.appendChild(textDeportivaMarca);
+function validarToyota () {
+    alert (toyota.Marca + " " + toyota.Modelo + " " + "El auto perfecto para salir de paseo con tu familia o amigos¡¡")
 
-    let modeloDeportiva = document.createElement("label");
-    contentDeportiva.appendChild(modeloDeportiva);
-    let textModelDeportiva = document.createTextNode(Deportiva.Modelo);
-    modeloDeportiva.appendChild(textModelDeportiva);
-
-    let precioDeportiva = document.createElement("label");
-    contentDeportiva.appendChild(precioDeportiva);
-    let textPrecioDeportiva = document.createTextNode(Deportiva.Precio);
-    precioDeportiva.appendChild(textPrecioDeportiva);
-
-    let imgDeportiva = document.createElement("img");
-    contentDeportiva.appendChild(imgDeportiva);
-    imgDeportiva.setAttribute("src", Deportiva.img)
+    mostrarToyota ();
 }
- let honda = new Motocicleta ("honda", "5", "$ 27.000.000", "img/moto_Honda.png");
- function validarHonda() {
-     alert (honda.Marca + " " + honda.Modelo + " " + "La moto mas eficaz en la carretera¡¡")
 
-     mostrarHonda();
+function mostrarToyota () {
+    let contenedorPrinToyota = document.getElementById("main_content");
+    let contentToyota        = document.createElement("div");
+    contenedorPrinToyota.appendChild(contentToyota);
+    contentToyota.setAttribute("class", "div");
+
+    let marcaToyota = document.createElement("label");
+    contentToyota.appendChild(marcaToyota);
+    let textToyotaMarca = document.createTextNode(toyota.Marca);
+    marcaToyota.appendChild(textToyotaMarca);
+    marcaToyota.setAttribute("class", "label1");
+
+
+    let modeloToyota = document.createElement("label");
+    contentToyota.appendChild(modeloToyota);
+    let textModelToyota = document.createTextNode(toyota.Modelo);
+    modeloToyota.appendChild(textModelToyota);
+    modeloToyota.setAttribute("class", "label2");
+
+
+    let precioToyota = document.createElement("label");
+    contentToyota.appendChild(precioToyota);
+    let textPrecioToyota = document.createTextNode(toyota.Precio);
+    precioToyota.appendChild(textPrecioToyota);
+    precioToyota.setAttribute("class", "label3");
+
+
+    let imgToyota = document.createElement("img");
+    contentToyota.appendChild(imgToyota);
+    imgToyota.setAttribute("src", toyota.Imagen)
+    imgToyota.setAttribute("class", "img");
+}
+ let chevrolet = new Automovil ("Celular Apple Iphone 12 (128gb)", "̶$̶4̶'̶3̶2̶8̶.̶2̶3̶5̶", "$2'679.000", "img/Iphone3.png");
+ function validarChevrolet() {
+     alert (chevrolet.Marca + " " + chevrolet.Modelo + " " + "El auto mas eficaz en la carretera¡¡")
+
+     mostarChevrolet();
  }
 
-function mostrarHonda () {
-    let contenedorPrincHonda = document.getElementById("moticos");
-    let contnedorHonda = document.createElement("div");
-    contenedorPrincHonda.appendChild(contnedorHonda);
+function mostarChevrolet () {
+    let contenedorPrincChevrolet = document.getElementById("main_content");
+    let contnedorChevrolet = document.createElement("div");
+    contenedorPrincChevrolet.appendChild(contnedorChevrolet);
+    contnedorChevrolet.setAttribute("class", "div");
 
-    let marcaHonda = document.createElement("label");
-    contnedorHonda.appendChild(marcaHonda);
-    let textMarcaHonda = document.createTextNode(honda.Marca);
-    marcaHonda.appendChild(textMarcaHonda);
+    let marcaChevrolet = document.createElement("label");
+    contnedorChevrolet.appendChild(marcaChevrolet);
+    let textMarcaChevrolet = document.createTextNode(chevrolet.Marca);
+    marcaChevrolet.appendChild(textMarcaChevrolet);
+    marcaChevrolet.setAttribute("class", "label1");
 
-    let modeloHonda = document.createElement("label");
-    contnedorHonda.appendChild(modeloHonda);
-    let textModeloHonda = document.createTextNode(honda.Modelo);
-    marcaHonda.appendChild(textModeloHonda);
+    let modeloChevrolet = document.createElement("label");
+    contnedorChevrolet.appendChild(modeloChevrolet);
+    let textModeloChevrolet = document.createTextNode(chevrolet.Modelo);
+    modeloChevrolet.appendChild(textModeloChevrolet);
+    modeloChevrolet.setAttribute("class", "label2");
 
-    let precioHonda = document.createElement("label");
-    contnedorHonda.appendChild(precioHonda);
-    let textPrecioHonda = document.createTextNode(honda.Precio);
-    precioHonda.appendChild(textPrecioHonda);
+    let precioChevrolet = document.createElement("label");
+    contnedorChevrolet.appendChild(precioChevrolet);
+    let textPrecioChevrolet = document.createTextNode(chevrolet.Precio);
+    precioChevrolet.appendChild(textPrecioChevrolet);
+    precioChevrolet.setAttribute("class", "label3");
 
-    let imgHonda = document.createElement("img");
-    contnedorHonda.appendChild(imgHonda);
-    imgHonda.setAttribute("src", honda.img)
+    let imgChevrolet = document.createElement("img");
+    contnedorChevrolet.appendChild(imgChevrolet);
+    imgChevrolet.setAttribute("src", chevrolet.Imagen)
+    imgChevrolet.setAttribute("class", "img");
 }
+
 window.addEventListener("keydown", function(event) {
-    let busqueda = document.getElementById("textBusqueda").value;
+    let busqueda = document.getElementById("textoBusqueda").value;
     if (event.key == "Enter") {
         limpiarVentana();
-        if (busqueda == "Motocicletas" ) {
-            mostrarMoto(moto);
-            mostrarDeportiva(Deportiva);
-            mostrarHonda(honda);
+        if (busqueda == "Iphone 12" ) {
+            mostrarVehiculo(auto);
+            mostrarToyota(toyota);
+            mostarChevrolet(chevrolet);
         }
-        else if (busqueda == "yamaha" ) {
-            mostrarMoto(moto);}
+        else if (busqueda == "Iphone 12" ) {
+            mostrarVehiculo(auto);}
         
 
-        else if (busqueda == "deportiva") {
-            mostrarDeportiva(Deportiva);}
+        if (busqueda == "Iphone 12 pro") {
+            mostrarToyota(toyota);}
 
-        else if (busqueda == "honda") {
-            mostrarHonda(honda);
+        else if (busqueda == "Iphone 12 128") {
+            mostarChevrolet(chevrolet);
         }
             
         else {
-            this.true;
+              this.true;
         } 
         return this. false.alert("no se han encontrado resultados."); 
     }
 })
+
+
 function limpiarVentana () {
-    document.getElementById("moticos").innerHTML = " " ;
-}
+    document.getElementById("main_content").innerHTML = "" ;
+}   
