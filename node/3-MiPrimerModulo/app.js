95b29074d316de3a1bfffea5d21a1785fb36c9d3
liliana <gicelagomez17082020@gmail.com>
2022-05-04 17:24:56 -0500

    add node

---
+++ node/3-MiPrimerModulo/app.js
@@ -0,0 +1,8 @@
+
+const math= require("./math.js");
+
+console.log(math.sumar(6,8));
+ console.log(math.restar(6,8));
+console.log(math.dividir(6,8));
+console.log(math.multiplicar(6,8)); 
+
---
+++ node/3-MiPrimerModulo/math.js
@@ -0,0 +1,29 @@
+let num1=6;
+let num2=8;
+
+
+
+function sumar( num1, num2) {
+        return (num1+num2) 
+}
+function restar( num1, num2) {
+    return (num1-num2) 
+}
+function dividir( num1, num2) {
+    return (num1/num2) 
+}
+function multiplicar( num1, num2) {
+    return (num1*num2) 
+}
+
+
+
+exports.sumar= sumar;
+exports.restar= restar;
+exports.dividir=dividir;
+exports.multiplicar=multiplicar;
+
+
+
+
+
